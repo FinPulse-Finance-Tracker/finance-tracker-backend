cd044dd949f06f387ce2089e7f34adde5a268296
nethmi02 <sathruwanihapuarachchi7@gmail.com>
2026-04-03 22:22:37 +0530

    fix: add AuthModule to UserModule and EmailIngestModule to resolve ClerkAuthGuard dependency

--- src/email-ingest/email-ingest.module.ts
+++ src/email-ingest/email-ingest.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { EmailIngestController } from './email-ingest.controller';
 import { EmailIngestService } from './email-ingest.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [PrismaModule, ConfigModule],
+    imports: [PrismaModule, ConfigModule, AuthModule],
     controllers: [EmailIngestController],
     providers: [EmailIngestService],
 })
